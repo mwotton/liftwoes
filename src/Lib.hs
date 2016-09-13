d40fb7e07c318dc8f1b46f3c03745c7290570395
Chris Allen <cma@bitemyapp.com>
2016-09-13 13:05:01 -0500

    Compiles with 7.10.3 now

--- src/Lib.hs
+++ src/Lib.hs
@@ -7,8 +7,7 @@ import qualified Data.Set                   as HS
 import qualified Data.Text                  as T
 import qualified Data.Text.IO               as T
 import           Data.Time
--- import           Instances.TH.Lift
-import           Instances
+import           Instances.TH.Lift
 import           Language.Haskell.TH.Syntax
 
 table = $(do r <- runIO (HS.fromList . T.lines  <$> T.readFile "/usr/share/dict/words")
